ffdb228391eff13a21fc00b261ba99998a844ce4
natnael-bedru <natnaelbedru@gmail.com>
2023-03-14 02:02:02 +0300

    Handling Errors Part-40

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,20 +3,30 @@ import { useState, useEffect } from "react";
 export const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
       // Simulate a network delay of 3 seconds
       setTimeout(async () => {
-        const response = await fetch(url);
-        const result = await response.json();
-        setLoading(false);
-        setData(result);
+        try {
+          const response = await fetch(url);
+          if(!response.ok) {
+            throw new Error (response.statusText);
+          }
+          const result = await response.json();
+          setLoading(false);
+          setData(result);
+          setError("");
+        } catch (error) {
+          setLoading(false);
+          setError(error.message);
+        }
       }, 3000);
     };
 
     fetchData();
   }, [url]);
-  return { data, loading };
+  return { data, loading, error };
 };
