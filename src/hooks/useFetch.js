ca1ed3bd0a7df6f57a3b81981784347568e3c999
natnael-bedru <natnaelbedru@gmail.com>
2023-03-14 01:39:40 +0300

    Loading State Part-39
    The loading screen has issue when it comes to sorting in available only the list is still there

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,16 +1,22 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export const useFetch = (url) => {
-    const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    useEffect( () => {
-        const fetchData = async () => {
-            const response = await fetch(url);
-            const result = await response.json();
-            setData(result);
-            console.log(result);
-        }
-        fetchData();
-    }, [url]);
-  return { data };
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      // Simulate a network delay of 3 seconds
+      setTimeout(async () => {
+        const response = await fetch(url);
+        const result = await response.json();
+        setLoading(false);
+        setData(result);
+      }, 3000);
+    };
+
+    fetchData();
+  }, [url]);
+  return { data, loading };
+};
