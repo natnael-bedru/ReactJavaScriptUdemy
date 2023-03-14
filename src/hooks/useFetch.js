fb7600f0aeaed554443c0a80a608681aea89d00b
natnael-bedru <natnaelbedru@gmail.com>
2023-03-14 17:13:47 +0300

    Aborting Fetch Request Part-41
    There is a commented case in the hooks/useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -6,12 +6,20 @@ export const useFetch = (url) => {
   const [error, setError] = useState("");
 
   useEffect(() => {
+    const controller = new AbortController();
+
     const fetchData = async () => {
       setLoading(true);
+      /*
+      => When the try catch statement is covered by the setTimeout function it responds like the following
+        *** Failed to execute 'fetch' on 'Window': The user aborted a request.
+      => If it is commented out it responds different as follows
+       *** The user aborted a request.
+     */
       // Simulate a network delay of 3 seconds
       setTimeout(async () => {
         try {
-          const response = await fetch(url);
+          const response = await fetch(url, { signal: controller.signal });
           if(!response.ok) {
             throw new Error (response.statusText);
           }
@@ -27,6 +35,7 @@ export const useFetch = (url) => {
     };
 
     fetchData();
+    return () => controller.abort();
   }, [url]);
   return { data, loading, error };
 };
